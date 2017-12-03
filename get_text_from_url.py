16ade4ff00f7523fb90580908887018539a59c78
Gwyn Jones <m@gwnjns.com>
2017-12-03 15:15:11 +0000

    Add remove_unwanted_elements function
    This is restricted to IDs only for the time being but can easily
    be extended to include classes.

--- get_text_from_url.py
+++ get_text_from_url.py
@@ -2,6 +2,7 @@ import urllib2
 from bs4 import BeautifulSoup
 import re
 
+
 def get_text_from_url(url, element, attribute, value):
     response = urllib2.urlopen(url)
     response_html = response.read()
@@ -26,7 +27,23 @@ def remove_extra_whitespace(text):
     return re.sub('\s+', ' ', text).strip()
 
 
+def remove_unwanted_elements(html):
+    # IDs only, for the time being
+    # See https://stackoverflow.com/questions/32063985/deleting-a-div-with-a-particlular-class-using-beautifulsoup
+    unwanted_ids = ['breadcrumb', 'breadcrumb-holder']
+
+    for id in unwanted_ids:
+
+        found_el = html.find('div', id=id)
+
+        if found_el:
+            found_el.decompose()
+
+    return html;
+
+
 def clean_html(html):
     html = remove_scripts(html)
     html = remove_styles(html)
+    html = remove_unwanted_elements(html)
     return html
