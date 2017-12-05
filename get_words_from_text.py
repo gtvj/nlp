9915586543e03fbd9058e9347cf467fb33fa81d3
Gwyn Jones <m@gwnjns.com>
2017-12-05 21:15:38 +0000

    Add list of regular expressions to be extracted with stopwords

--- get_words_from_text.py
+++ get_words_from_text.py
@@ -8,10 +8,13 @@ def extract_words_without_stopwords(text):
 
     my_stopwords = ['...', '+44', '\-', '\'s', 'The']
 
+    my_patterns = ['\d{1,4}', '[A-Z]{2,3}', '-', '/']
+
     custom_stopwords = set(stopwords.words('english') + list(punctuation) + my_stopwords)
 
     words_without_stopwords = ' '.join([word for word in word_tokenize(text) if word not in custom_stopwords])
 
-    words_without_stopwords_or_dates = re.sub('\d{1,4}', ' ', words_without_stopwords).strip()
+    for pattern in my_patterns:
+        words_without_stopwords = re.sub(pattern, ' ', words_without_stopwords).strip()
 
-    return words_without_stopwords_or_dates
+    return words_without_stopwords
