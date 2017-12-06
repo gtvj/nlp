ad03710d2e79f4fd67b8aad0bb568dd75ccada3e
Gwyn Jones <m@gwnjns.com>
2017-12-06 21:45:18 +0000

    Test several details pages at once

--- find_related_research_guides.py
+++ find_related_research_guides.py
@@ -3,10 +3,33 @@ import json
 from details_page import DetailsPage
 from ngrams import find_ngram_match
 
-page = DetailsPage('http://discovery.nationalarchives.gov.uk/details/r/C259')
+pages = [
+    'http://discovery.nationalarchives.gov.uk/details/r/C259',
+    'http://discovery.nationalarchives.gov.uk/details/r/C260'
+]
 
-decorated_guides = json.loads(open('generated/decorated_guides.json', 'r').read())
+for page in pages:
+    page = DetailsPage(page)
 
-print page.summary
-find_ngram_match(page.trigrams, decorated_guides, 'trigrams')
-find_ngram_match(page.bigrams, decorated_guides, 'bigrams')
+    decorated_guides = json.loads(open('generated/decorated_guides.json', 'r').read())
+
+    print '----------'
+    print '---Page---'
+    print '----------'
+
+    print page.summary
+    print page.trigrams
+    print page.bigrams
+
+    print '-----------------------------'
+    print '---Guides found by trigram---'
+    print '-----------------------------'
+
+    for i in find_ngram_match(page.trigrams, decorated_guides, 'trigrams'):
+        print i
+
+    print '-----------------------------'
+    print '---Guides found by bigram---'
+    print '-----------------------------'
+    for i in find_ngram_match(page.bigrams, decorated_guides, 'bigrams'):
+        print i
